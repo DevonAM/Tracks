8a6a49b9b91235a9e43faf7a22d7760eca982977
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-24 14:35:49 -0600

    connect to mongo instance

--- tracks-server/src/index.js
+++ tracks-server/src/index.js
@@ -1,11 +1,28 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Hello World!");
-})
+const mongoUri =
+  "mongodb+srv://admin:passwordpassword@cluster0.9wlkx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(3000, () => {
-    console.log("listening on port 3000");
-})
+  console.log("listening on port 3000");
+});
