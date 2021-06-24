e181116388c5debfdbc87d80d08b79f9f467be04
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-24 17:17:54 -0600

    add logic for saving new user server side

--- tracks-server/src/routes/authRoutes.js
+++ tracks-server/src/routes/authRoutes.js
@@ -4,8 +4,15 @@ const mongoose = require("mongoose");
 const Users = mongoose.model("User");
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-  console.log(req.body);
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new Users({ email, password });
+    await user.save();
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
   res.send("Sent a signup post");
 });
 
