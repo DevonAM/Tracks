5c3cb918643898c1300981a303f22ffa53ab62b9
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-07 14:21:01 -0600

    add inputs with state

--- tracks-front/src/screens/SignupScreen.js
+++ tracks-front/src/screens/SignupScreen.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Button, Input } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Signup for Tracks</Text>
       </Spacer>
-      <Input label="Email" />
+      <Input
+        label="Email"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={email}
+        onChangeText={setEmail}
+      />
       <Spacer />
-      <Input label="Password" />
+      <Input
+        label="Password"
+        secureTextEntry
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={setPassword}
+      />
       <Spacer>
         <Button title="Signup" />
       </Spacer>
