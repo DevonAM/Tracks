2ba60f950b12dcb7202c99f9a1b02aa23da7b10b
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-06 18:52:31 -0600

    complete routes for tracks add and get

--- tracks-server/src/index.js
+++ tracks-server/src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("./models/User");
+require("./models/Track");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const bodyParser = require("body-parser");
 const requireAuth = require("./middlewares/requireAuth");
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
   "mongodb+srv://admin:passwordpassword@cluster0.9wlkx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
