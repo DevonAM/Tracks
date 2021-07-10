b917fe1bc56b1e29e68fc22c6bb40d6af091a731
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-10 13:03:02 -0600

    store jwt in asyncstorage and dispatch  to reducer

--- tracks-front/src/context/AuthContext.js
+++ tracks-front/src/context/AuthContext.js
@@ -1,10 +1,13 @@
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
       return { ...state, errorMessage: action.payload };
+    case "signup":
+      return { errorMessage: "", token: action.payload };
     default:
       return state;
   }
@@ -14,7 +17,8 @@ const signup = (dispatch) => {
   return async ({ email, password }) => {
     try {
       const response = await trackerApi.post("/signup", { email, password });
-      console.log(response.data);
+      await AsyncStorage.setItem("token", response.data.token);
+      dispatch({ type: "signup", payload: response.data.token });
     } catch (err) {
       console.log(err.message);
       dispatch({
