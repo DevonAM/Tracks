2bebf905b9e3fe64cdde5660f75dac80210407a5
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-10 14:52:13 -0600

    Create navigation helper and navigate after signup

--- tracks-front/App.js
+++ tracks-front/App.js
@@ -8,7 +8,7 @@ import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
-
+import { setNavigator } from "./src/navigationRef";
 import { Provider as AuthProvider } from "./src/context/AuthContext";
 
 const switchNavigator = createSwitchNavigator({
@@ -32,7 +32,7 @@ const App = createAppContainer(switchNavigator);
 export default () => {
   return (
     <AuthProvider>
-      <App />
+      <App ref={setNavigator} />
     </AuthProvider>
   );
 };
