9d1a35f906471b1d3c46b53f51ae7a2564f71adf
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-15 12:10:55 -0600

    add safe area view to bring content below status bar

--- tracks-front/src/screens/AccountScreen.js
+++ tracks-front/src/screens/AccountScreen.js
@@ -3,16 +3,17 @@ import { View, Text, StyleSheet } from "react-native";
 import { Button } from "react-native-elements";
 import { Context as AuthContext } from "../context/AuthContext";
 import Spacer from "../components/Spacer";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
   return (
-    <>
-      <Text style={{ fontSize: 30 }}>AccountScreen</Text>
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
       <Spacer>
         <Button title="Sign Out" onPress={signout} />
       </Spacer>
-    </>
+    </SafeAreaView>
   );
 };
 
