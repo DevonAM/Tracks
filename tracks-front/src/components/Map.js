44a520d0878e3c06839cecb668950081a075f695
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-17 15:37:33 -0600

    add mapview to Map component

--- tracks-front/src/components/Map.js
+++ tracks-front/src/components/Map.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 const Map = () => {
-  return (
-    <View>
-      <Text>I am a map</Text>
-    </View>
-  );
+  return <MapView style={styles.map} />;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+});
 
 export default Map;
