8d10381ef00023112bd6d237dad409597681b3e8
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-18 17:11:09 -0600

    extract logic into custom hook

--- tracks-front/src/screens/TrackCreateScreen.js
+++ tracks-front/src/screens/TrackCreateScreen.js
@@ -1,45 +1,15 @@
 import "../_mockLocation";
-import React, { useEffect, useState, useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { Text, StyleSheet } from "react-native";
 import Map from "../components/Map";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {
-  requestForegroundPermissionsAsync,
-  watchPositionAsync,
-  Accuracy,
-} from "expo-location";
+import useLocation from "../hooks/useLocation";
 
 import { Context as LocationContext } from "../context/LocationContext";
-
 const TrackCreateScreen = () => {
-  const [err, setErr] = useState(null);
   const { addLocation } = useContext(LocationContext);
+  const [err] = useLocation(addLocation);
 
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        (location) => {
-          addLocation(location);
-          console.log(location);
-        }
-      );
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
-  useEffect(() => {
-    startWatching();
-  }, []);
   return (
     <SafeAreaView forcedInset={{ top: "always" }}>
       <Text style={{ fontSize: 30 }}>TrackCreateScreen</Text>
