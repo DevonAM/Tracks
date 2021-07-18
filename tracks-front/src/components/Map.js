e1782893b895735bf64cf335ae17ce89da90c44d
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-18 11:01:36 -0600

    style mapview and add polyline with hardcoded points

--- tracks-front/src/components/Map.js
+++ tracks-front/src/components/Map.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
-  return <MapView style={styles.map} />;
+  let points = [];
+
+  for (let i = 0; i < 20; i++) {
+    points.push({
+      latitude: 37.297817 + i * 0.001,
+      longitude: -113.02877 + i * 0.001,
+    });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.297817,
+        longitude: -113.02877,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      }}
+    >
+      <Polyline coordinates={points} />
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
