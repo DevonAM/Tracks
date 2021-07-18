654f2801c7b1263e5455d491fcf5df65a0e9e4e8
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-18 13:12:48 -0600

    request location permissions

--- tracks-front/src/screens/TrackCreateScreen.js
+++ tracks-front/src/screens/TrackCreateScreen.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Map from "../components/Map";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { requestForegroundPermissionsAsync } from "expo-location";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
   return (
     <SafeAreaView forcedInset={{ top: "always" }}>
       <Text style={{ fontSize: 30 }}>TrackCreateScreen</Text>
       <Map />
+      {err ? <Text>{err}</Text> : null}
     </SafeAreaView>
   );
 };
