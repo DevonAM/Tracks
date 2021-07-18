9a0053b659dcf843083e771d8b065a9ceb41facc
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-18 11:01:58 -0600

    cleanup imports

--- tracks-front/src/screens/SigninScreen.js
+++ tracks-front/src/screens/SigninScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
 import { Context } from "../context/AuthContext";
@@ -7,6 +7,7 @@ import { NavigationEvents } from "react-navigation";
 
 const SigninScreen = () => {
   const { state, signin, clearErrorMessage } = useContext(Context);
+
   return (
     <View style={styles.container}>
       <NavigationEvents onWillFocus={clearErrorMessage} />
