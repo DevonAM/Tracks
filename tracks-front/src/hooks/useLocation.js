a2c6d6ae5bb64c60ef5636714336c287631a054a
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-19 12:51:36 -0600

    disable tracking when user leaves page
    use withNavigationFocus to get isFocused prop

--- tracks-front/src/hooks/useLocation.js
+++ tracks-front/src/hooks/useLocation.js
@@ -5,15 +5,17 @@ import {
   Accuracy,
 } from "expo-location";
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
+  const [subscriber, setSubscriber] = useState(null);
+
   const startWatching = async () => {
     try {
       const { granted } = await requestForegroundPermissionsAsync();
       if (!granted) {
         throw new Error("Location permission not granted");
       }
-      await watchPositionAsync(
+      const sub = await watchPositionAsync(
         {
           accuracy: Accuracy.BestForNavigation,
           timeInterval: 1000,
@@ -21,13 +23,21 @@ export default (callback) => {
         },
         callback
       );
+
+      setSubscriber(sub);
     } catch (e) {
       setErr(e);
     }
   };
 
   useEffect(() => {
-    startWatching();
-  }, []);
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      //stop tracking
+      subscriber.remove();
+      setSubscriber(null);
+    }
+  }, [shouldTrack]); //watch for change in shouldTrack
   return [err];
 };
--- tracks-front/src/screens/TrackCreateScreen.js
+++ tracks-front/src/screens/TrackCreateScreen.js
@@ -2,17 +2,17 @@ import "../_mockLocation";
 import React, { useContext } from "react";
 import { Text, StyleSheet } from "react-native";
 import Map from "../components/Map";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, withNavigationFocus } from "react-navigation";
+import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
 
-import { Context as LocationContext } from "../context/LocationContext";
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(addLocation);
+  const [err] = useLocation(isFocused, addLocation);
 
   return (
     <SafeAreaView forcedInset={{ top: "always" }}>
-      <Text style={{ fontSize: 30 }}>TrackCreateScreen</Text>
+      <Text style={{ fontSize: 30 }}>Create A Track</Text>
       <Map />
       {err ? <Text>{err.message}</Text> : null}
     </SafeAreaView>
@@ -21,4 +21,5 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+//withNavigationFocus gives prop to TrackCreateScreen "isFocused"
+export default withNavigationFocus(TrackCreateScreen);
