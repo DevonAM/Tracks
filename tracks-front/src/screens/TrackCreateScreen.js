0a760ee5b34e0b467a48705f97454145040e8504
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-19 15:25:58 -0600

    useCallback to prevent callback being rebuilt

--- tracks-front/src/screens/TrackCreateScreen.js
+++ tracks-front/src/screens/TrackCreateScreen.js
@@ -1,14 +1,25 @@
 import "../_mockLocation";
-import React, { useContext } from "react";
+import React, { useContext, useCallback } from "react";
 import { Text, StyleSheet } from "react-native";
 import Map from "../components/Map";
+import TrackForm from "../components/TrackForm";
 import { SafeAreaView, withNavigationFocus } from "react-navigation";
 import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(isFocused, addLocation);
+  const {
+    state: { recording },
+    addLocation,
+  } = useContext(LocationContext);
+  //only rebuild the callback if recording has changed
+  const callback = useCallback(
+    (location) => {
+      addLocation(location, recording);
+    },
+    [recording]
+  );
+  const [err] = useLocation(isFocused || recording, callback);
 
   return (
     <SafeAreaView forcedInset={{ top: "always" }}>
