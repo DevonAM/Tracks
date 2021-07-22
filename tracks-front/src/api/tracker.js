ba45d1a9cd3144b8b9f310b49fa9bfa9077936ed
Devon Acree-Meza <dacreeme@asu.edu>
2021-07-22 11:01:42 -0600

    add automatic authentication

--- tracks-front/src/api/tracker.js
+++ tracks-front/src/api/tracker.js
@@ -1,6 +1,24 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //base url must be changed to match active url from ngrok
-export default axios.create({
-  baseURL: "http://e5530cd5a96b.ngrok.io",
+const instance = axios.create({
+  baseURL: "http://a56d3aa92797.ngrok.io",
 });
+
+//modify the config if there is a token
+//automatic authentication
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return new Promise.reject(err);
+  }
+);
+
+export default instance;
